5c73eb831721b6447d8f6b88786fa84f114f7513
Kamilla <mis.kamillka@gmail.com>
2026-01-29 09:07:13 +0000

    Custom hooks

---
+++ src/hooks/useTasks.js
@@ -0,0 +1,102 @@
+import { useState, useEffect, useRef, useCallback, useMemo } from "react";
+import useTasksLocalStorage from "./useTasksLocalStorage";
+
+const useTasks = () => {
+  const { savedTasks, saveTasks } = useTasksLocalStorage();
+
+  const [tasks, setTasks] = useState(
+    savedTasks ?? [
+      {
+        id: "task-1",
+        title: "Купить корм",
+        isDone: false,
+      },
+      {
+        id: "task-2",
+        title: "Погладить кота",
+        isDone: true,
+      },
+    ],
+  );
+
+  const [newTaskTitle, setNewTaskTitle] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
+
+  const newTaskInputRef = useRef(null);
+
+  const deleteAllTasks = useCallback(() => {
+    const isConfirmed = confirm("Are you sure you want to delete all?");
+    if (isConfirmed) {
+      setTasks([]);
+    }
+  }, []);
+
+  const deleteTask = useCallback(
+    (taskId) => {
+      setTasks(tasks.filter((task) => task.id !== taskId));
+    },
+    [tasks],
+  );
+
+  const toggleTaskComplete = useCallback(
+    (taskId, isDone) => {
+      setTasks(
+        tasks.map((task) => {
+          if (task.id === taskId) {
+            return { ...task, isDone };
+          }
+          return task;
+        }),
+      );
+    },
+    [tasks],
+  );
+
+  const addTask = useCallback(() => {
+    if (newTaskTitle.trim().length > 0) {
+      const newTask = {
+        id: crypto?.randomUUID() ?? Date.now().toString(),
+        title: newTaskTitle,
+        isDone: false,
+      };
+      setTasks((prev) => [...prev, newTask]);
+      setNewTaskTitle("");
+      setSearchQuery("");
+      newTaskInputRef.current.focus();
+    }
+  }, [newTaskTitle]);
+
+  useEffect(() => {
+    saveTasks(tasks);
+  }, [tasks]);
+
+  useEffect(() => {
+    newTaskInputRef.current.focus();
+  }, []);
+
+  const filteredTask = useMemo(() => {
+    const clearSearchQuery = searchQuery.trim().toLowerCase();
+
+    return clearSearchQuery.length > 0
+      ? tasks.filter(({ title }) =>
+          title.toLowerCase().includes(clearSearchQuery),
+        )
+      : null;
+  }, [searchQuery, tasks]);
+
+  return {
+    tasks,
+    filteredTask,
+    deleteAllTasks,
+    deleteTask,
+    toggleTaskComplete,
+    newTaskTitle,
+    setNewTaskTitle,
+    searchQuery,
+    setSearchQuery,
+    newTaskInputRef,
+    addTask,
+  };
+};
+
+export default useTasks;
